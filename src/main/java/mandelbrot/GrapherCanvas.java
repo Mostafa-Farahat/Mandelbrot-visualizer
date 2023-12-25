2e7267301d5052529917f5d8e774c39a9e6f7eea
Mostafa-Farahat <mostafa.farahat78@gmail.com>
2023-12-26 01:00:43 +0200

    optimized number of calls to .rgb() by bundling pixels with same colorNumber first closes #1

--- src/main/java/mandelbrot/GrapherCanvas.java
+++ src/main/java/mandelbrot/GrapherCanvas.java
@@ -1,58 +1,73 @@
 package mandelbrot;
-
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import java.util.ArrayList;
 
-import java.util.Timer;
 
 public class GrapherCanvas extends Canvas {
     final double WIDTH;
     final double HEIGHT;
-
+    int numberOfColorBrackets;
     Set set;
+    GraphicsContext gc = this.getGraphicsContext2D();
+    private class Pixel{
+        int x;
+        int y;
+        Pixel(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
 
+    ArrayList<ArrayList<Pixel>> colorBrackets = new ArrayList<ArrayList<Pixel>>();
     public GrapherCanvas(Set set, double width, double height){
         super(width,height);
         WIDTH = width;
         HEIGHT = height;
         this.set = set;
+        numberOfColorBrackets = set.getEscapeCount();
+
+        for(int i=0; i<=numberOfColorBrackets; i++){
+            colorBrackets.add(new ArrayList<Pixel>());
+        }
     }
 
-
-    GraphicsContext gc = this.getGraphicsContext2D();
-
-    //renders the set
-    //takes ranges desired for render, the value of escape (to modify accuracy)
-    //and the graphicsContext for the canvas
+    /*renders the set
+    takes ranges desired for render, the value of escape (to modify accuracy)
+    and the graphicsContext for the canvas*/
     public void renderSet(double xMin,  double xMax, double yMin, double yMax){
         double stepSize = Math.max((xMax-xMin) / WIDTH,(yMax-yMin) / HEIGHT );
 
-        //these are the actual values that are
-        //used for computations
-        //they are the numerical value assigned to each pixel
-        //(the numerical value of each pixel after scaling)
+        /*these are the actual values that are
+        used for computations
+        they are the numerical value assigned to each pixel
+        (the numerical value of each pixel after scaling)*/
         double xPoint =xMin;
         double yPoint = yMin;
 
         for(int x=0; x<=WIDTH; x++){
             for(int y=0; y<=HEIGHT; y++){
+
                 Complex c = new Complex(xPoint, yPoint);
                 int colorNumber = set.isInSet(c);
+                colorBrackets.get(colorNumber).add(new Pixel(x,y));
 
-                if(colorNumber == 0){
-                    gc.setFill(Color.BLACK);
-
-                }else{
-                    gc.setFill(Color.WHITE);
-                }
-
-                gc.fillRect(x, y, 1, 1);
                 yPoint = yPoint + stepSize;
             }
             xPoint = xPoint + stepSize;
             yPoint = yMin;
         }
-    }
 
+        for(int i=0; i<=numberOfColorBrackets;i++){
+            if (i == 0) {
+                gc.setFill(Color.rgb(0,0,0));
+            }else{
+                gc.setFill(Color.rgb(255,255,255));
+            }
+            for(Pixel pixel : colorBrackets.get(i)){
+                gc.fillRect(pixel.x,pixel.y,1,1);
+            }
+        }
+    }
 }
