ef979efcd392ceb459255bd6a94fb1c98671d028
Mostafa-Farahat <mostafa.farahat78@gmail.com>
2023-12-25 15:40:01 +0200

    added the set interface to allow program to graphdifferent escape time algorithm fractals

---
+++ src/main/java/mandelbrot/Set.java
@@ -0,0 +1,13 @@
+package mandelbrot;
+
+/*
+* When plotting other sets that use escape time algorithm(ex:julia set)
+* a set object implementing this interface should be created
+* the interface will be used by the grapher class to graph the
+* set regardless of the rules that determine the set
+*/
+
+public interface Set {
+    //return number of iterations for escape
+    int isInSet(Complex c, int escapeCount);
+}
